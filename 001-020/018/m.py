cdfa4ac6f4cbb8007c07055dbd39d7260c4ac992
C1 <zj175@139.com>
2022-05-25 19:28:47 +0800

    【更新题解】:018识别有效的IP地址和掩码并进行分类统计

--- 001-020/018/m.py
+++ 001-020/018/m.py
@@ -6,7 +6,12 @@ import re
 A, B, C, D, E, errs, privates = 0, 0, 0, 0, 0, 0, 0
 
 
-def getBin(string):
+def get_bin(string):
+    """
+    将点分十进制的IP地址转成二进制的形式
+    :param string:点分十进制的IP地址字符串
+    :return:物理层的二进制序列字符串
+    """
     string_bin = ''
     for i in string.split('.'):
         string_bin += bin(int(i))[2:].rjust(8, '0')
@@ -16,7 +21,7 @@ def getBin(string):
 try:
     while True:
         ip, mask = input().split('~')
-        mask_bin = getBin(mask)
+        mask_bin = get_bin(mask)
         if ip.split('.')[0] in ('0', '127'):
             continue
         elif mask in ('0.0.0.0', '255.255.255.255'):
@@ -26,24 +31,24 @@ try:
             if re.search('01', mask_bin):
                 errs += 1
                 continue
-        ip_bin = getBin(ip)
+        ip_bin = get_bin(ip)
         if re.search(r'\.\.', ip):
             errs += 1
-        elif getBin('1.0.0.0') < ip_bin < getBin('126.255.255.255'):
+        elif get_bin('1.0.0.0') < ip_bin < get_bin('126.255.255.255'):
             A += 1
-            if getBin('10.0.0.0') < ip_bin < getBin('10.255.255.255'):
+            if get_bin('10.0.0.0') < ip_bin < get_bin('10.255.255.255'):
                 privates += 1
-        elif getBin('128.0.0.0') < ip_bin < getBin('191.255.255.255'):
+        elif get_bin('128.0.0.0') < ip_bin < get_bin('191.255.255.255'):
             B += 1
-            if getBin('172.16.0.0') < ip_bin < getBin('172.31.255.255'):
+            if get_bin('172.16.0.0') < ip_bin < get_bin('172.31.255.255'):
                 privates += 1
-        elif getBin('192.0.0.0') < ip_bin < getBin('223.255.255.255'):
+        elif get_bin('192.0.0.0') < ip_bin < get_bin('223.255.255.255'):
             C += 1
-            if getBin('192.168.0.0') < ip_bin < getBin('192.168.255.255'):
+            if get_bin('192.168.0.0') < ip_bin < get_bin('192.168.255.255'):
                 privates += 1
-        elif getBin('224.0.0.0') < ip_bin < getBin('239.255.255.255'):
+        elif get_bin('224.0.0.0') < ip_bin < get_bin('239.255.255.255'):
             D += 1
-        elif getBin('240.0.0.0') < ip_bin < getBin('255.255.255.255'):
+        elif get_bin('240.0.0.0') < ip_bin < get_bin('255.255.255.255'):
             E += 1
 except (EOFError, ValueError):
     pass
