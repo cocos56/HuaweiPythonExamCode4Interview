277b86e1be251cb3d6ad87696b18e557fbdbd2f5
C1 <zj175@139.com>
2022-05-25 16:40:41 +0800

    【新增题解】:005进制转换

---
+++ 001-020/005/m2.py
@@ -0,0 +1,25 @@
+"""
+题解2：逐位计算值然后累加
+"""
+s = input()
+p = 0
+total = 0
+for i in s[::-1]:
+    if i == 'x':
+        break
+    elif i == 'A':
+        total += 10 * pow(16, p)
+    elif i == 'B':
+        total += 11 * pow(16, p)
+    elif i == 'C':
+        total += 12 * pow(16, p)
+    elif i == 'D':
+        total += 13 * pow(16, p)
+    elif i == 'E':
+        total += 14 * pow(16, p)
+    elif i == 'F':
+        total += 15 * pow(16, p)
+    else:
+        total += int(i) * pow(16, p)
+    p += 1
+print(total)
