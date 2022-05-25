900a1dc2e5083279b0f74ee40e66fc5714c8974f
C1 <zj175@139.com>
2022-05-25 12:13:26 +0800

    【更新题解】【001字符串最后一个单词的长度】

--- 001-020/001/m2_1.py
+++ 001-020/001/m2_1.py
@@ -1,10 +1,16 @@
 """
 题解2_1：先逆序再处理_使用reverse函数
 """
-s = input()
-length = 0
-for i in reversed(s):
-    if i == ' ':
-        break
-    length += 1
-print(length)
+
+
+def get_length():
+    s = input()
+    length = 0
+    for i in reversed(s):
+        if i == ' ':
+            break
+        length += 1
+    print(length)
+
+
+get_length()
