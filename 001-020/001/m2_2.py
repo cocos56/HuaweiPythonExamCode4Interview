74b1a84b71cab225cfc815e23b2d335a0fea0b06
C1 <zj175@139.com>
2022-05-25 12:53:55 +0800

    【更新题解】【001字符串最后一个单词的长度】

--- 001-020/001/m2_2.py
+++ 001-020/001/m2_2.py
@@ -2,9 +2,9 @@
 题解2_2：先逆序再处理_字符串切片功能逆转字符串
 """
 s = input()
-Length = 0
+LENGTH = 0
 for i in s[::-1]:
     if i == ' ':
         break
-    Length += 1
-print(Length)
+    LENGTH += 1
+print(LENGTH)
