1509bf2a5e0b6d6f7a1fd422968390cd3287279d
C1 <zj175@139.com>
2022-05-25 19:20:31 +0800

    【更新题解】:017坐标移动

--- 001-020/017/m3.py
+++ 001-020/017/m3.py
@@ -5,9 +5,9 @@ x, y = 0, 0
 for i in input().split(';'):
     if not 2 <= len(i) <= 3:
         continue
-    elif not '0' <= i[1] <= '9':
+    if not '0' <= i[1] <= '9':
         continue
-    elif len(i) == 3 and not '0' <= i[2] <= '9':
+    if len(i) == 3 and not '0' <= i[2] <= '9':
         continue
     direction, step = i[0], int(i[1:])
     if direction == 'A':
