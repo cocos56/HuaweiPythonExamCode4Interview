2cfc89a9b5eb2b94cccf979e9970d23adcd680f3
C1 <zj175@139.com>
2022-05-25 19:38:49 +0800

    【新增题解】:019简单错误记录

---
+++ 001-020/019/m.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+dic = {}
+try:
+    while True:
+        p, n = input().split(' ')
+        name = p.split('\\')[-1][-16:]
+        key = name + ' ' + n
+        if key in dic:
+            dic[key] += 1
+        else:
+            dic[key] = 1
+except (EOFError, ValueError):
+    keys = [k for k in dic][-8:]
+    for k in keys:
+        print(k, dic[k])
